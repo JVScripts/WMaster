e66c699aebc91dbf9f47bb560bf0f617b147dd62
JVScripts <ayfeel1@gmail.com>
2026-08-29 17:10:16 +0200

    1.4

--- open_cards.user.js
+++ open_cards.user.js
@@ -9,7 +9,7 @@
 // @run-at       document-idle
 // @noframes
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/JVScripts/WMaster/refs/heads/main/open_cards.user.js
-// @downloadURL  https://raw.githubusercontent.com/JVScripts/WMaster/refs/heads/main/open_cards.user.js
-// @require      https://raw.githubusercontent.com/JVScripts/WMaster/refs/heads/main/open_cards.js
+// @updateURL    https://raw.githubusercontent.com/JVScripts/WMaster/raw/refs/heads/main/open_cards.user.js
+// @downloadURL  https://raw.githubusercontent.com/JVScripts/WMaster/raw/refs/heads/main/open_cards.user.js
+// @require      https://raw.githubusercontent.com/JVScripts/WMaster/raw/refs/heads/main/open_cards.js
 // ==/UserScript==
